680d198a3d706c446e12d57218d7d5f653ce0d83
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-04-01 14:36:14 +0530

    updated the backend

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
