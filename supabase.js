40bb563b710bc413855647ee19658476d951567c
mintureddy25 <chappetasaitejareddy@gmail.com>
2025-04-01 14:29:40 +0530

    updated the backend

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const SUPABASE_URL = 'https://jzrisvwferxhpijyiqcr.supabase.co';
-const SUPABASE_SERVICE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6cmlzdndmZXJ4aHBpanlpcWNyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mjk5NjI2NSwiZXhwIjoyMDU4NTcyMjY1fQ.aYuKB-hxpyBctGIohgGIm3p4ZaTgs7xB08i4kMhMlF4';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
 module.exports ={
